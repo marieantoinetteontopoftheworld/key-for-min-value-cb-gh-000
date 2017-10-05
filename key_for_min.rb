a7b65048779c7583cabc2f6d0e82bf3d762e04de
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-05 14:01:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   key_with_lowest_val = nil
 
   name_hash.each do |key, val|
-    if val == nil || val < lowest_val
+    if lowest_val == nil || val < lowest_val
       lowest_val = val
       key_with_lowest_val = key
     end
